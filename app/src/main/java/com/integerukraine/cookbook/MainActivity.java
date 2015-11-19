76cad70e7858c3d46b7987f102f4d166ebc7ff8c
andrew <andriytheprogrammer@gmail.com>
2015-11-19 10:31:59 +0200

    Grid mostly done. Resolved #1

--- app/src/main/java/com/integerukraine/cookbook/MainActivity.java
+++ app/src/main/java/com/integerukraine/cookbook/MainActivity.java
@@ -1,17 +1,15 @@
 package com.integerukraine.cookbook;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.integerukraine.cookbook.adapter.MainGridAdapter;
 import com.integerukraine.cookbook.parse.ParseKey;
--- app/src/main/java/com/integerukraine/cookbook/adapter/MainGridAdapter.java
+++ app/src/main/java/com/integerukraine/cookbook/adapter/MainGridAdapter.java
@@ -1,6 +1,7 @@
 package com.integerukraine.cookbook.adapter;
 
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -9,7 +10,6 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 
 import com.bumptech.glide.Glide;
 import com.integerukraine.cookbook.R;
@@ -20,6 +20,8 @@ import com.parse.ParseObject;
 import org.json.JSONException;
 
 import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by andrew on 17.11.15.
@@ -44,25 +46,64 @@ public class MainGridAdapter extends RecyclerView.Adapter<MainGridAdapter.TrackV
     public void onBindViewHolder(TrackViewHolder holder, int position) {
         try {
             ParseObject recipe = recipes.get(position);
-            holder.tvDescription.setText(recipe.getString(ParseKey.Recipe.DESCRIPTION));
-            holder.tvDishName.setBackgroundColor(Color.parseColor(recipe.getParseObject(ParseKey.Recipe.GRID_IMAGE).getString(ParseKey.Image.COLOR)));
-            holder.tvDishName.setText(recipe.getString(ParseKey.Recipe.DISH_NAME));
-            if (position < columns) {
-                FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) holder.cardRecipe.getLayoutParams();
-                layoutParams.topMargin = Convertations.dpToPx(8, holder.itemView.getContext());
-                holder.cardRecipe.setLayoutParams(layoutParams);
-            }
-            Glide.with(holder.itemView.getContext())
-                    .load(recipe.getParseObject(ParseKey.Recipe.GRID_IMAGE).getString(ParseKey.Image.URL))
-                    .centerCrop()
-                    .placeholder(R.color.colorAccent)
-                    .override(recipe.getParseObject(ParseKey.Recipe.GRID_IMAGE).getJSONArray(ParseKey.Image.RESOLUTION).getInt(0),
-                            recipe.getParseObject(ParseKey.Recipe.GRID_IMAGE).getJSONArray(ParseKey.Image.RESOLUTION).getInt(1))
-                    .crossFade()
-                    .into(holder.imageRecipe);
+            initSizes(recipe, holder, position);
+            initFonts(recipe, holder, position);
+            initTextViews(recipe, holder, position);
+            initImages(recipe, holder, position);
+
+
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Used to work with sizes of view
+     */
+    private void initSizes(ParseObject recipe, TrackViewHolder holder, int position) {
+        if (position < columns) {
+            FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) holder.cardRecipe.getLayoutParams();
+            layoutParams.topMargin = Convertations.dpToPx(8, holder.itemView.getContext());
+            holder.cardRecipe.setLayoutParams(layoutParams);
+        }
+    }
+    /**
+     * Used to work with fonts of view
+     */
+    private void initFonts(ParseObject recipe, TrackViewHolder holder, int position) {
+        Typeface arialBlack = Typeface.createFromAsset(holder.itemView.getContext().getAssets(), "fonts/arial_black.ttf");
+        Typeface rubik300 = Typeface.createFromAsset(holder.itemView.getContext().getAssets(), "fonts/rubik_300.ttf");
+
+        holder.tvDishCalories.setTypeface(arialBlack);
+        holder.tvDishTime.setTypeface(arialBlack);
+        holder.tvDishType.setTypeface(arialBlack);
+        holder.tvUsername.setTypeface(arialBlack);
+
+        holder.tvDescription.setTypeface(rubik300);
+        holder.tvDishCalories.setTypeface(rubik300);
+        holder.tvDishName.setTypeface(rubik300);
+    }
+
+    private void initTextViews(ParseObject recipe, TrackViewHolder holder, int position) {
+//        Setting texts
+        holder.tvDescription.setText(recipe.getString(ParseKey.Recipe.DESCRIPTION));
+        holder.tvDishName.setText(recipe.getString(ParseKey.Recipe.DISH_NAME));
+
+//        Setting colors & images
+        holder.tvDishName.setBackgroundColor(Color.parseColor(recipe.getParseObject(ParseKey.Recipe.GRID_IMAGE).getString(ParseKey.Image.COLOR)));
+
+    }
+
+    private void initImages(ParseObject recipe, TrackViewHolder holder, int position) throws JSONException {
+        Glide.with(holder.itemView.getContext())
+                .load(recipe.getParseObject(ParseKey.Recipe.GRID_IMAGE).getString(ParseKey.Image.URL))
+                .centerCrop()
+                .placeholder(R.color.colorAccent)
+                .override(recipe.getParseObject(ParseKey.Recipe.GRID_IMAGE).getJSONArray(ParseKey.Image.RESOLUTION).getInt(0),
+                        recipe.getParseObject(ParseKey.Recipe.GRID_IMAGE).getJSONArray(ParseKey.Image.RESOLUTION).getInt(1))
+                .crossFade()
+                .into(holder.imageRecipe);
     }
 
 
@@ -79,17 +120,24 @@ public class MainGridAdapter extends RecyclerView.Adapter<MainGridAdapter.TrackV
 
 
     class TrackViewHolder extends RecyclerView.ViewHolder {
-        TextView tvDescription;
-        TextView tvDishName;
+        TextView tvDishName, tvDescription, tvDishType, tvDishCalories, tvDishTime, tvUsername, tvUserStatus;
         ImageView imageRecipe;
+        CircleImageView imageUserAvatar;
         CardView cardRecipe;
+
 
         public TrackViewHolder(View itemView) {
             super(itemView);
 
-            tvDescription = (TextView) itemView.findViewById(R.id.tv_description);
             tvDishName = (TextView) itemView.findViewById(R.id.tv_dish_name);
+            tvDescription = (TextView) itemView.findViewById(R.id.tv_dish_description);
+            tvDishType = (TextView) itemView.findViewById(R.id.tv_dish_type);
+            tvDishCalories = (TextView) itemView.findViewById(R.id.tv_dish_calories);
+            tvDishTime = (TextView) itemView.findViewById(R.id.tv_dish_time);
+            tvUsername = (TextView) itemView.findViewById(R.id.tv_username);
+            tvUserStatus = (TextView) itemView.findViewById(R.id.tv_user_status);
             imageRecipe = (ImageView) itemView.findViewById(R.id.image_recipe);
+            imageUserAvatar = (CircleImageView) itemView.findViewById(R.id.image_user_avatar);
             cardRecipe = (CardView) itemView.findViewById(R.id.card_recipe);
         }
     }
